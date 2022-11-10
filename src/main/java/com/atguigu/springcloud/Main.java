04f1ad9adbc9d65b1336782412487d1f79d451a0
liulang <1574937505@qq.com>
2022-11-10 18:01:09 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/springcloud/Main.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author LiuLang
+ * @version 1.0
+ * Create by 2022/11/10 17:59
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好哈哈哈哈");
+    }
+}
